8808267d74b8aeb791b6b031829a53a47bb4d192
Geoff Greer <geoff@greer.fm>
2013-06-21 01:39:01 -0700

    Jslint

--- flegmatic.js
+++ flegmatic.js
@@ -8,24 +8,38 @@ var _ = require("underscore");
 var watchers = {};
 var current_state = {};
 
-var main = function(){
-  var cwd = process.cwd();
-  console.log('watching cwd', cwd);
-  watch(cwd);
+
+var listener = function (original_path, is_dir, event, filename) {
+  var buf;
+  if (!is_dir && event !== 'rename') {
+    buf = fs.readFileSync(original_path);
+    console.log(buf.toString(), current_state[original_path].toString());
+  }
+  console.log(event, original_path, is_dir);
 };
 
-var watch = function(to_watch){
+var add_listener = function (f, is_dir) {
+  is_dir = is_dir === true ? true : false;
+  fs.readFile(f, function (err, buf) {
+    current_state[f] = buf;
+    // note sure possibly 50K closures is a good idea, but it works for HN...
+    fs.watch(f, listener.bind(null, f, is_dir));
+  });
+
+};
+
+var watch = function (to_watch) {
   var sub_dirs = [],
     files = [],
     iter,
-    children = fs.readdirSync(to_watch).map(function(child){
+    children = fs.readdirSync(to_watch).map(function (child) {
       return path.join(to_watch, child);
     });
 
-  iter = function(p, cb){
+  iter = function (p, cb) {
     console.log(p);
-    fs.lstat(p, function(err, stats){
-      if (stats.isDirectory()){
+    fs.lstat(p, function (err, stats) {
+      if (stats.isDirectory()) {
         sub_dirs.push(p);
       } else if (stats.isFile()) {
         files.push(p);
@@ -34,28 +48,18 @@ var watch = function(to_watch){
     });
   };
 
-  async.eachLimit(children, 10, iter, function(err){
+  async.eachLimit(children, 10, iter, function (err) {
     sub_dirs.forEach(watch);
     files.forEach(add_listener);
     add_listener(to_watch, true);
   });
 };
 
-var add_listener = function(f, is_dir){
-  is_dir = is_dir === true ? true : false;
-  fs.readFile(f, function(err, buf){
-    current_state[f] = buf;
-    // note sure possibly 50K closures is a good idea, but it works for HN...
-    fs.watch(f, listener.bind(null, f, is_dir));
-  });
 
+var main = function () {
+  var cwd = process.cwd();
+  console.log('watching cwd', cwd);
+  watch(cwd);
 };
 
-var listener = function(original_path, is_dir, event, filename){
-  if (!is_dir && event != 'rename'){
-    buf = fs.readFileSync(original_path);
-    console.log(buf.toString(), current_state[original_path].toString());
-  }
-  console.log(event, original_path, is_dir);
-};
 main();
