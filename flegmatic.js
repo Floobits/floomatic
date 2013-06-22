ed3f1b9dde01a05bd76a75bc6eea8c19b8804b43
Matt Kaniaris <mkaniaris@gmail.com>
2013-06-21 18:31:02 -0700

    set defaults on command line flags

--- flegmatic.js
+++ flegmatic.js
@@ -19,7 +19,7 @@ var parse_url = function (url, cb) {
   parsed_url.host = "floobits.com";
   parsed_url.port = 3448;
   parsed_url.klass = tls;
-  parsed_url.room = "rax-demo";
+  parsed_url.workspace = "rax-demo";
   parsed_url.owner = "kansface";
 
   // parsed_url.conn_class = net;
@@ -53,17 +53,31 @@ var parse_floorc = function(){
   return floorc;
 };
 
+var parse_dot_floo = function(){
+  var parsed_url = {};
+  try {
+    floo_file = fs.readFileSync(".floo");
+    data = JSON.parse(floo_file);
+    parsed_url = parse_url(data.url);
+  } catch (e) {
+    console.log("no .floo file in current directory");
+  }
+  return parsed_url;
+};
+
 var parse_args = function(){
   var floorc = parse_floorc();
+  var parsed_url = parse_dot_floo();
 
   return optimist
-    .usage('Usage: $0 -h [HOST] -p [PORT] -o [OWNER] -s [SECRET] -u [username] --create-room --delete-room')
-    .default('h', 'floobits.com')
+    .usage('Usage: $0 -o [owner] -w [workspace] -u [username] -s [secret] -H [host] -p [port] --create-room --delete-room')
+    .default('H', parsed_url.host || 'floobits.com')
     .default('p', 3448)
     .default('u', floorc.username)
-    .default('o', floorc.username)
     .default('s', floorc.secret)
-    .demand(['h','p', 'o', 's', 'u'])
+    .default('w', parsed_url.workspace)
+    .default('o', parsed_url.owner || floorc.username)
+    .demand(['H', 'p', 'o', 'w', 'u', 's'])
     .argv;
 };
 
@@ -77,18 +91,8 @@ exports.run = function () {
     parsed_url,
     args = parse_args();
 
-  try {
-    floo_file = fs.readFileSync(".floo");
-    data = JSON.parse(floo_file);
-    parsed_url = parse_url(data.url);
-  } catch (e) {
-    console.log("no .floo file in current directory");
-  }
-
-
-  floo_conn = new floo_connection.FlooConnection(parsed_url);
+  floo_conn = new floo_connection.FlooConnection(args.H, args.p, args.o, args.w, args.u, args.s);
   console.log('watching cwd', process.cwd());
   floo_listener = new listener.Listener(process.cwd(), floo_conn);
-
-  floo_conn.connect(args.u, args.s);
+  floo_conn.connect();
 };
